79b7d568a8c958210d1988bcf900c30b7b5b7324
Gustavo Lopes <lopes.gustavojf@gmail.com>
2023-06-16 10:09:52 -0300

    fix opengraph url

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -25,7 +25,7 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
-        <meta content={`https://reactresume.com${pathname}`} property="og:url" />
+        <meta content={`https://gustavo-lopes.vercel.app${pathname}`} property="og:url" />
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
         <meta content={title} name="twitter:title" />
