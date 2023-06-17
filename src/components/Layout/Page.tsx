b54d4040ffdab4709f6df5860c4c57f4418560d7
Gustavo Lopes <lopes.gustavojf@gmail.com>
2023-06-17 19:50:38 -0300

    fix favicon

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -18,6 +18,9 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <link href={`https://www.gustavo-lopes.me${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/favicon.ico" rel="icon" sizes="any" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+            
         <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="/site.webmanifest" rel="manifest" />
 
