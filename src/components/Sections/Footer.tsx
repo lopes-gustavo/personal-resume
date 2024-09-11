12aaddf365462458a9ab041d0069b885a728bb6e
Gustavo Lopes <lopes.gustavojf@gmail.com>
2024-09-11 17:32:40 -0300

    Remove Copyright

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -18,7 +18,6 @@ const Footer: FC = memo(() => (
       <div className="flex gap-x-4 text-neutral-500">
         <Socials />
       </div>
-      <span className="text-sm text-neutral-700">© Copyright 2023 Gustavo Lopes</span>
     </div>
   </div>
 ));
