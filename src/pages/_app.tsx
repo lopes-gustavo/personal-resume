e52edd336f1ce3ed220f56846831810b1bdfa3d6
Gustavo Lopes <lopes.gustavojf@gmail.com>
2024-09-12 16:10:58 -0300

    Add SpeedInsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
